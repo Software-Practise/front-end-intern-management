749f3d105b21738d9c121fedb02a418b37b03d36
cameronKC <cameronleekcmo@gmail.com>
2022-02-03 18:25:36 -0600

    UI dependencies / updated Student Admin Views
    tailwind css ui kit dashboard template and relevant dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./views/AdminDashboard";
 import StudentDashboard from "./views/StudentDashboard";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <AdminDashboard />
-      <StudentDashboard />
-    </div>
+    <Router>
+      <Routes>
+        {/* Todo: Private Routing For Admin Dashboard */}
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-center">
+      <h1>
+        <a href="/admin">Admin View</a>
+      </h1>
+      <h1>
+        <a href="/student">Student View</a>
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
