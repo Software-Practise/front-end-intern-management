cef6976d9133605fce245dc5357a97d2e8b3c733
tbry3467 <tbry3467@users.noreply.github.com>
2022-03-03 14:52:36 -0600

    Faculty Application View Created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./views/AdminDashboard";
 import StudentDashboard from "./views/StudentDashboard";
 import FacultyDashboard from "./views/FacultyDashboard";
+import FacultyApplicationView from "./views/FacultyApplicationView";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/faculty" element={<FacultyDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/facultyApplication" element={<FacultyApplicationView />} />
       </Routes>
     </Router>
   );
