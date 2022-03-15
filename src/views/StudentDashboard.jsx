bccaf020c2e21e220321cde4ce6fe3f3438d1df6
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-03-15 10:53:56 -0500

    Changed default student page

--- src/views/StudentDashboard.jsx
+++ src/views/StudentDashboard.jsx
@@ -11,8 +11,8 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Apply", href: "#", current: true },
-  { name: "Applications", href: "#", current: false },
+  { name: "Applications", href: "#", current: true },
+  { name: "Apply Now", href: "#", current: false },
   { name: "Review", href: "#", current: false },
   { name: "Offers", href: "#", current: false },
   { name: "Get Help", href: "#", current: false },
