25974305b6219cbb4aa4b2aa7ce88069d51f1d55
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-03-17 14:17:16 -0500

    Create files for other student pages

--- src/views/StudentDashboard.jsx
+++ src/views/StudentDashboard.jsx
@@ -14,7 +14,6 @@ const user = {
 const navigation = [
   { name: "Applications", href: "#", current: true },
   { name: "Apply Now", href: "#", current: false },
-  { name: "Review", href: "#", current: false },
   { name: "Offers", href: "#", current: false },
   { name: "Get Help", href: "#", current: false },
 ];
