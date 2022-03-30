fb85b30035c19aa01af098965c04f2a6e2803290
BWeddle <72270236+BWeddle@users.noreply.github.com>
2022-03-30 13:22:51 -0500

    Begin Creating Application

--- src/views/StudentApply.jsx
+++ src/views/StudentApply.jsx
@@ -203,13 +203,90 @@ export default function Example() {
 
 				<header className="bg-gray-800 shadow">
 					<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-						<h1 className="text-3xl font-bold text-nwgreen">Student Apply</h1>
+						<h1 className="text-3xl font-bold text-nwgreen">Student Internship Application</h1>
 					</div>
 				</header>
 				<main>
 					<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
 						<div className="px-4 py-6 sm:px-0">
-							putting application type things here
+							<form className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-40">
+								<div className="text-nwgreen text-2xl flex justify-center border-b-2 py-2 mb-4">
+									{""}
+									Internship Application
+								</div>
+								<div className="mb-4">
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Email:
+									</label>
+									<input 
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="email"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Department:
+									</label>
+									<input
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="dept"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Street Address:
+									</label>
+									<input 
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="street"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										City:
+									</label>
+									<input
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="city"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										State:
+									</label>
+									<input 
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="state"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Zip Code:
+									</label>
+									<input
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="zipCode"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Phone Number:
+									</label>
+									<input 
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										name="phoneNumber"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Major:
+									</label>
+									<input
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										major="fname"
+									/>
+									<label className="block text-gray-700 text-sm font-normal mb-2">
+										Level:
+									</label>
+									<input
+										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+										placeholder="Graduate/Undergraduate"
+										major="level"
+									/>
+								</div>
+								<div className="flex justify-center items-center">
+									<button className="px-4 text- py-2 rounded text-white inline-block shadow-lg bg-nwgreen">
+										Submit
+									</button>
+								</div>
+								
+							</form>
 						</div>
 						{/* /End replace */}
 					</div>
