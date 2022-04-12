80c17257ac53eb99ef827a55b8c96419e24315f6
Sam Ritter <54680543+SRitt12@users.noreply.github.com>
2022-04-12 15:11:43 -0500

    Add dummy searchbar to admin app list

--- src/views/Admin/AdminApplicationList.jsx
+++ src/views/Admin/AdminApplicationList.jsx
@@ -7,66 +7,66 @@ import TableData from '.././falculty.json';
 
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+	name: "Tom Cook",
+	email: "tom@example.com",
+	imageUrl:
+		"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "/Admin", current: false },
-  { name: "Applications", href: "/AdminApplicationList", current: false },
-  { name: "Dev Link Page", href: "/", current: true},
+	{ name: "Dashboard", href: "/Admin", current: false },
+	{ name: "Applications", href: "/AdminApplicationList", current: false },
+	{ name: "Dev Link Page", href: "/", current: true },
 ];
 const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "/login" },
+	{ name: "Your Profile", href: "#" },
+	{ name: "Settings", href: "#" },
+	{ name: "Sign out", href: "/login" },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+	return classes.filter(Boolean).join(" ");
 }
 
-function DynamicTable(){
+function DynamicTable() {
 	// get table column
 	const column = Object.keys(TableData[0]);
-  
+
 	//get table heading data
-	const ThData =()=>{
-	  return column.map((data)=>{
-		return <th key={data} className="m-4 border border-slate-600 ...">{data}</th>
-	  })
+	const ThData = () => {
+		return column.map((data) => {
+			return <th key={data} className="m-4 border border-slate-600 ...">{data}</th>
+		})
 	}
-  
+
 	//get table row data
-	const tdData =() =>{
-	  return TableData.map((data)=>{
-		return(
-		  <tr>
-			{
-			  column.map((v)=>{
-				return <td className="border border-slate-700 ...">{data[v]}</td>
-			  })
-			}
-		  </tr>
-		)
-	  })
+	const tdData = () => {
+		return TableData.map((data) => {
+			return (
+				<tr>
+					{
+						column.map((v) => {
+							return <td className="border border-slate-700 ...">{data[v]}</td>
+						})
+					}
+				</tr>
+			)
+		})
 	}
-  
+
 	return (
-	  <table className="border-collapse mt-3 border border-slate-500 ...">
-		<thead>
-		  <tr>{ThData()}</tr>
-		</thead>
-		<tbody>
-		  {tdData()}
-		</tbody>
-	  </table>
+		<table className="border-collapse mt-3 border border-slate-500 ...">
+			<thead>
+				<tr>{ThData()}</tr>
+			</thead>
+			<tbody>
+				{tdData()}
+			</tbody>
+		</table>
 	)
-  }
+}
 
 export default function Example() {
-  return (
+	return (
 		<>
 			{/*
         This example requires updating your template:
@@ -262,7 +262,7 @@ export default function Example() {
 					{/* Header Below Green Navbar, with "Faculty Dashboard" inside */}
 					<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
 						<h1 className="text-3xl font-bold text-nwgreen">
-							Faculty/Admin Applications Overview{" "}
+							Admin Applications Overview{" "}
 						</h1>
 					</div>
 				</header>
@@ -270,7 +270,18 @@ export default function Example() {
 					{" "}
 					{/* Everything Below "Faculty Dashboard" */}
 					<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-						<div> Search bar to filter results</div>
+						<div className="flex justify-left">
+							<div className="mb-3 xl:w-96">
+								<div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
+									<input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2"/>
+										<button className="btn inline-block px-6 py-2.5 bg-blue-600 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+											<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+												<path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+											</svg>
+										</button>
+								</div>
+							</div>
+						</div>
 						<div class="max-w-2xl mx-auto"> </div>
 
 						{/* start of table */}
